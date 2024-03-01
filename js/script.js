3b0d8f6449e2c438b526dd0c79cb39a6100c4ae3
Aetherys-sudo <andreilungoci832@yahoo.com>
2024-03-01 19:20:22 +0200

    Updated cookie button JS script and transition

--- js/script.js
+++ js/script.js
@@ -8,3 +8,15 @@ questions.forEach((x) => {
     
 });
 
+const cookieClick = document.querySelector(".cookie-click");
+const popUp = document.querySelector(".cookie-window");
+const x = document.querySelector(".cookie-x");
+
+cookieClick.addEventListener("click", function(e) {
+    popUp.classList.toggle("active-cookie");
+    //document.body.style.background = "linear-gradient(0deg, rgba(0, 0, 0, 0.9) 0%, rgba(8, 8, 8, 0.8) 40%, rgba(0,0,0,0.9) 100%)";
+});
+
+x.addEventListener("click", function(e) {
+    popUp.classList.toggle("active-cookie");
+});
