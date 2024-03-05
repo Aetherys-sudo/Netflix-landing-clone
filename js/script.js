e557894bff5e5be427c4be5418bb069b75774231
Aetherys-sudo <andreilungoci832@yahoo.com>
2024-03-05 18:48:01 +0200

    Updated Cookie menu
    Updated the logic for the cookie menu fully and made some small tweaks on the language dropdown.

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ questions.forEach((x) => {
 const cookieClick = document.querySelector(".cookie-click");
 const popUp = document.querySelector(".cookie-window");
 const x = document.querySelector(".cookie-x");
+const submitBtn = document.querySelector(".cookie-save");
 
 cookieClick.addEventListener("click", function(e) {
     popUp.classList.toggle("active-cookie");
@@ -23,4 +24,37 @@ x.addEventListener("click", function(e) {
     popUp.classList.toggle("active-cookie");
     popUp.classList.remove("move");
     popUp.classList.add("dont-move");
+});
+
+submitBtn.addEventListener("click", function(e) {
+    popUp.classList.toggle("active-cookie");
+    popUp.classList.remove("move");
+    popUp.classList.add("dont-move");
+});
+
+const cookieCat = [...document.querySelectorAll(".cookie-category")];
+const content = [...document.querySelectorAll(".content")];
+const fillers = [...document.querySelectorAll(".fillers")];
+
+
+cookieCat.forEach((x, index) => {
+    x.addEventListener("click", function(e) {
+        x.children[0].classList.add("filler-active");
+        x.classList.add("cat-active");
+        cookieCat.forEach((y) => {
+            if (x != y) {
+                y.children[0].classList.remove("filler-active");
+                y.classList.remove("cat-active");
+            }
+        });
+
+        content.forEach((el, indexEl) => {
+            el.classList.add("hide");
+            el.classList.remove("unhide");
+            if (indexEl == index) {
+                el.classList.add("unhide");
+                el.classList.remove("hide");
+            }
+        });
+    });
 });
